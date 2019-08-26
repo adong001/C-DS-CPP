d041f0419ec998ccb7346107ca072c06933b2951
=‘E=ME=adong001 <1792095378@qq.com>
2019-08-26 21:04:02 +0800

    提交代码

--- C++_Test0825/C++_Test0825/main.cpp
+++ C++_Test0825/C++_Test0825/main.cpp
@@ -20,42 +20,100 @@ using namespace std;//声明使用库提供的std名字空间内的库函数
 //		}
 //	}
 //}
-void fun(int a = 0)
-{
-	cout << a << endl;
-}
+//void fun(int a = 0)
+//{
+//	cout << a << endl;
+//}
+//
+//void fun1(int a = 10,int b=20,int c=  30)
+//{
+//	cout << a << endl;
+//	cout << b << endl; 
+//	cout << c << endl;
+//}
+//void fun2(int a = 10 , int b , int c = 30)
+//{
+//	cout << a << endl;
+//	cout << b << endl;
+//	cout << c << endl;
+//}
+//void fun3(int a , int b , int c = 30)
+//{
+//	cout << a << endl;
+//	cout << b << endl;
+//	cout << c << endl;
+//}
+//void fun4(int a , int b, int c =10);
+//
+//void fun4(int a , int b , int c =10 )
+//{
+//	cout << a << endl;
+//	cout << b << endl;
+//	cout << c << endl;
+//}
 
-void fun1(int a = 10,int b=20,int c=  30)
+//int add(int a, int b)
+//{
+//	return a + b;
+//}
+//
+//double add(double a, double b)
+//{
+//	return a + b;
+//}
+//short Add(short left, short right) { return left + right; }
+//
+//int Add(short left, short right) { return left + right; }
+
+void Swap(int &aa, int &bb)
 {
-	cout << a << endl;
-	cout << b << endl; 
-	cout << c << endl;
+	int tmp = aa;
+	aa = bb;
+	bb = tmp;
 }
-void fun2(int a , int b = 20, int c = 30)
-{
-	cout << a << endl;
-	cout << b << endl;
-	cout << c << endl;
-}
-void fun3(int a , int b , int c = 30)
-{
-	cout << a << endl;
-	cout << b << endl;
-	cout << c << endl;
-}
-void fun4(int a , int b , int c )
-{
-	cout << a << endl;
-	cout << b << endl;
-	cout << c << endl;
+ inline int Add(int a, int b) 
+{ 
+	int c = a + b;
+    return c;
 }
 int main()
 {
-	fun(10);
-	fun();
-	fun2(200, 300);
-	//cout <<N1::add(2, 5);//std是系统提供的名字空间，cout自动识别类型打印
-	//cout <<N0:: N1::add(2, 5);//名字空间可嵌套
+	int ret = Add(2, 4);
+	/*int& ret = Add(1, 2);
+	Add(3, 4);
+	cout << "Add(1, 2) is :" << ret << endl;*/
+
+	/*int a = 10;
+	int& ra = a;
+	ra = 20;
+	int* pa = &a;
+	*pa = 20;*/
+	/*int a = 10;
+	int b = 20;
+	cout << "a=" << a << "  b=" << b << endl;
+	Swap(a, b);
+	cout << "a=" << a << "  b=" << b << endl;*/
+	//int a = 10;
+	//int &b = a;
+	//int &c = a;
+	//cout << "a = " << a << " b = " << b << " c = " << c << endl;
+
+	/*int a = 10;
+	int b = 20;
+	int &c = a;
+	c = b;
+	*/
+	//const int a = 10;
+	//int &A = a;  //编译出错，a为常量
+	//int &B = 100;//编译出错，100为常量
+	//int c = 100;
+	//float &C = c;//编译出错，c和C类型不同
+	/*int CaiXuKun = 100;
+	int &KunKun = CaiXuKun;
+	int &
+	cout << "CaiXuKun = " << CaiXuKun << " KunKun = " << KunKun << endl;
+	cout << "&CaiXuKun = " << &CaiXuKun << " &KunKun = " << &KunKun << endl;*/
 	system("pause");
-    return 0;
+	return 0;
 }
+
---
+++ C++_Test0826/C++_Test0826/main.cpp
@@ -0,0 +1,81 @@
+﻿#include<iostream>
+using namespace std;
+
+void Swap(int &aa, int &bb)//aa是a的引用(别名)，bb是b的引用(别名)
+{
+	int tmp = aa;
+	aa = bb;
+	bb = tmp;
+}
+//引用特性
+//1. 引用在定义时必须初始化 
+//2. 一个变量可以有多个引用 
+//3. 引用一旦引用一个实体，再不能引用其他实体
+
+
+void TestRef()
+{
+	int a = 10;
+	//int& ra;   // 该条语句编译时会出错 
+	int& ra = a;
+	int& rra = a;
+	printf("%p  %p  %p\n", &a, &ra, &rra);
+}
+void TestConstRef()
+{
+	const int a = 10;
+	//int& ra = a;   // 该语句编译时会出错，a为常量 
+	const int& ra = a;
+	// int& b = 10;  // 该语句编译时会出错，b为常量 
+	const int& b = 10;
+	double d = 12.34;
+	//int& rd = d;  // 该语句编译时会出错，类型不同 
+	const int& rd = d;
+}
+
+int& add(int a, int b)
+{
+	int c = a + b;
+	return c;//反回了局部变量的引用
+}
+
+//void(auto &a, auto &b)//auto不能作为函数参数和返回值类型
+//{
+//	return a + b;
+//}
+int main()
+{
+	int i = 10;
+	char j = 'j';
+
+	auto a = i;//C++11版本才有auto
+	cout << typeid(a).name() << endl;
+
+	auto b = 'j';
+	cout << typeid(b).name() << endl;
+
+	auto c = &j;
+	cout << typeid(c).name() << endl;
+
+	auto d = 10;
+	cout << typeid(d).name() << endl;
+
+	auto *e = &i;
+	cout << typeid(e).name() << endl;
+	//auto ar[] = { 1, 2, 3, 4 };//报错，auto不能用来声明数组
+	//auto aa = 1, bb = 2.33;//报错，一行只能判断一种类型
+	//int likui = 100;
+	//int &heixuanfeng = likui;//定义时为引用(取别名)，否则为取地址
+	//cout << "likui = " << likui << " heixaunfeng = " << heixuanfeng << endl;//值相等
+	//cout << "&likui = " << &likui << " &heixaunfeng = " << &heixuanfeng << endl;//地址相同，两者是一个东西，两个名字而已。
+	//int a = 10, b = 20;
+	//cout << "a = " << a << " b = " << b << endl;
+	//Swap(a, b);
+	//cout << "a = " << a << " b = " << b << endl;
+	//int &ret = add(1, 2);//ret值为随机值
+	//add(3, 4);
+	//cout << "add(1,2)=" << ret << endl;
+
+	system("pause");
+	return 0;
+}
