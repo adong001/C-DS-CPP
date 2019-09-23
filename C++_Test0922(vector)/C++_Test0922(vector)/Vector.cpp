3f06eb20659bb4956a2c537617ec3c3b9a1b2990
=‘E=ME=adong001 <1792095378@qq.com>
2019-09-23 20:39:42 +0800

    1

--- C++_Test0922(vector)/C++_Test0922(vector)/Vector.cpp
+++ C++_Test0922(vector)/C++_Test0922(vector)/Vector.cpp
@@ -11,3 +11,16 @@
 //{
 //
 //}
+
+//Bit::Vector<T>::Vector(size_t n, const T& val )
+//{
+//	m_start(nullptr);
+//	m_end(nullptr);
+//	m_endOfStorage(nullptr);
+//}
+//
+//
+//void Vector<T>::Reserve(size_t n)
+//{
+//
+//}
