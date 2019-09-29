f9f188d18299c84a3a26eb4766b474e7d4c69b19
=‘E=ME=adong001 <1792095378@qq.com>
2019-09-29 20:47:34 +0800

    提交代码

--- C++_Tetst0927(list)/C++_Tetst0927(list)/Iterator.h
+++ C++_Tetst0927(list)/C++_Tetst0927(list)/Iterator.h
@@ -1,27 +1,27 @@
-#pragma once
-#include"ListNode.h"
-template<class T, class Ref, class Ptr>// T T* T&
-class ListIterator
-{
-private:
-	typedef ListNode<T>* m_PNode;
-	typedef ListIterator<T, Ref, Ptr> m_Self;
-	m_PNode m_pNode;
-public:
-	ListIterator(m_PNode pNode = nullptr) :
-		m_pNode(pNode)
-	{}
-
-	ListIterator(const m_Self& res) :
-		m_pNode(res.m_pNode)
-	{}
-
-	T& operator*();
-	T* operator->();
-	Self& operator++();
-	Self operator++(int);
-	Self& operator--();
-	Self& operator--(int);
-	bool operator!=(const Self& l);
-	bool operator==(const Self& l);
-};
+//#pragma once
+//#include"ListNode.h"
+//template<class T, class Ref, class Ptr>// T T* T&
+//class ListIterator
+//{
+//private:
+//	typedef ListNode<T>* m_PNode;
+//	typedef ListIterator<T, Ref, Ptr> m_Self;
+//	m_PNode m_pNode;
+//public:
+//	ListIterator(m_PNode pNode = nullptr) :
+//		m_pNode(pNode)
+//	{}
+//
+//	ListIterator(const m_Self& res) :
+//		m_pNode(res.m_pNode)
+//	{}
+//
+//	T& operator*();
+//	T* operator->();
+//	Self& operator++();
+//	Self operator++(int);
+//	Self& operator--();
+//	Self& operator--(int);
+//	bool operator!=(const Self& l);
+//	bool operator==(const Self& l);
+//};
--- C++_Tetst0927(list)/C++_Tetst0927(list)/List.h
+++ C++_Tetst0927(list)/C++_Tetst0927(list)/List.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+﻿//#pragma once
 #include<iostream>
 //#include"ListNode.h"
 #include"Iterator.h"
