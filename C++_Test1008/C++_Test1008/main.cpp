2ac7926133846ee98c1f79dc04c4905100124ae7
=‘E=ME=adong001 <1792095378@qq.com>
2019-10-12 23:24:50 +0800

    1

--- C++_Test1008/C++_Test1008/main.cpp
+++ C++_Test1008/C++_Test1008/main.cpp
@@ -20,15 +20,6 @@ using namespace std;
 //minStack.getMin();   --> ·µ»Ø - 2.
 
 
-/**
-* Your MinStack object will be instantiated and called as such:
-* MinStack* obj = new MinStack();
-* obj->push(x);
-* obj->pop();
-* int param_3 = obj->top();
-* int param_4 = obj->getMin();
-*/
-
 
 class MinStack
 {
@@ -77,6 +68,7 @@ public:
 		return m_MinData.top();
 	}
 };
+
 int main()
 {
 	MinStack minStack ;
