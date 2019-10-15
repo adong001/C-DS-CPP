ffc490d46385ea04f9c84eb53299d0354ecf9ebf
=‘E=ME=adong001 <1792095378@qq.com>
2019-10-15 23:28:28 +0800

    1

--- C++_Test1014/C++_Test1014/t1.cpp
+++ C++_Test1014/C++_Test1014/t1.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<cmath>
+#include<algorithm>
 #include<vector>
 using namespace std;
 //输入一个数，判断最多使用几部变成斐波那契数(只能给这个数+1或-1)
@@ -10,20 +12,25 @@ class Solution {
 public:
 	size_t BecomeFibSteps(size_t num)
 	{
-		int step = 0;//步数
-		vector<int> v;
-		int a = 1;
-		int b = 1;
-		v.push_back(1);
-		v.push_back(1);
-		while(num > )
+		int fn, fn_1 = 0, fn_2 = 1;
+		fn = fn_1 + fn_2;
+		while (fn < num)//找到比num大的或相等的那个斐波那契数
+		{
+			fn_2 = fn_1;
+			fn_1 = fn;
+			fn = fn_1 + fn_2;
+		}  
+		int a = fn - num;
+		int b = fn_1 - num;
+
+		return min(abs(a), abs(b));
 
 	}
-};
-int main()
+}; //1 2 3 5 8 13 21 
+int main1()
 {   
 	Solution s;
-	cout << s.BecomeFibSteps(15) << endl;
+	cout << s.BecomeFibSteps(4) << endl;
     system("pause");
     return 0;
 }
