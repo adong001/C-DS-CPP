5ec53ea295aeb0ef8bbc986dbed224bd7238be81
=‘E=ME=adong001 <1792095378@qq.com>
2019-10-26 19:07:46 +0800

    1

--- C++_Test1015(二叉树遍历)/C++_Test1015(二叉树)/Tree.h
+++ C++_Test1015(二叉树遍历)/C++_Test1015(二叉树)/Tree.h
@@ -217,9 +217,10 @@ public:
 		TreeNode<T>* cur = m_root;
 		qtmp.push(cur);//先将根节点push进去
 
-		while (cur)
+		while (!qtmp.empty())
 		{
-
+			cur = qtmp.front();
+			qtmp.pop();
 			vres.push_back(cur->m_val);
 			if (cur->m_left)
 			{
@@ -230,85 +231,272 @@ public:
 			{
 				qtmp.push(cur->m_right);
 			}
-
-			qtmp.pop();
-			if (!qtmp.empty())
-			{
-				cur = qtmp.front();
-			}
-			else
-			{
-				cur = nullptr;
-			}
 		}
 		return vres;
 
 	}
-	vector<T> LevelOrder_Recursive(size_t LevelNodeNums, TreeNode<T>* root)//层序递归版
+
+	size_t TreeDepth()//求树的深度,需要递归，就得传参，只能重载一个函数封装一下
 	{
-		static vector<T> vres;
-		size_t Num;//这层的节点数
-		int count = 0;
+		return TreeDepth(m_root);
+	}
+
+	size_t TreeDepth(TreeNode<T>* root)//求树的深度
+	{
 		if (!root)
+		{
+			return 0;
+		}
+		int LeftChildDepth = TreeDepth(root->m_left);
+		int RightChildDepth = TreeDepth(root->m_right);
+		return LeftChildDepth > RightChildDepth ? LeftChildDepth + 1 : RightChildDepth + 1;
+	}
+	// 递归实现层序遍历
+	//由于层序遍历是一种广度优先的遍历，所以我们需要通过层数控制整个函数的运行
+	//每轮循环传入要输出的层数
+	//然后当层数不等于0时，向下递归，每次递归层数减一
+	//当层数等于0的时候，则说明已经到达了要输出的那一层，递归的结束条件就满足了，直接输出值即可
+	//vector<T> LevelOrder_Recursive()
+	//{
+	//	size_t LevelNum = TreeDepth();
+	//	vectoc<T> vres;
+	//	vres = LevelOrder_Recursive(m_root, LevelNum,vres);
+	//	return vres;
+	//}
+
+	//vector<T> LevelOrder_Recursive(TreeNode<T>* root, int LevelNum, vector<T> vtmp)
+	//{
+	//	vector<T> vres = vtmp;
+	//	if (!root)
+	//	{
+	//		vres.push_back(root->m_val);//根不为空，先把根push进去
+	//        if(LevelNum > 1)
+	//		{
+	//			LevelOrder_Recursive(root->m_left, LevelNum - 1, vres);
+	//			LevelOrder_Recursive(root->m_right, LevelNum - 1,vres);
+	//		}
+	//	}
+	//	return vres;
+	//}
+
+	vector<int> LevelNodeNums()//求每层节点个数
+	{
+		queue<TreeNode<T>*> qtmp;
+		vector<int> vres;//模板特换为int，存节点个数
+		if (!m_root)
 		{
 			return vres;
 		}
-		vres.push_back(root->m_val);
-		for (Num = 0; Num < LevelNodeNums; Num++)
+		int length = 1;
+		TreeNode<T>* cur = m_root;
+		qtmp.push(cur);//先将根节点push进去
+		while (!qtmp.empty())
 		{
-			if (root)
+			vres.push_back(length);
+			for (int i = 0; i < length; i++)
 			{
-				if (root->m_left->m_left)
+				cur = qtmp.front();
+				qtmp.pop();
+				if (cur->m_left)
 				{
-					count++;
+					qtmp.push(cur->m_left);
 				}
-				if (root->m_left->m_right)
+
+				if (cur->m_right)
 				{
-					count++;
+					qtmp.push(cur->m_right);
 				}
-				LevelOrder_Recursive(count, root->m_left);
-				count = 0;
-				if (root->m_right->m_left)
-				{
-					count++;
-				}
-				if (root->m_right->m_right)
-				{
-					count++;
-				}
-				LevelOrder_Recursive(count,root->m_right);
-			}
+			}//for循环结束，就代表一层结束
+			length = qtmp.size();
 		}
-	return vres;
-}
-	/ 递归实现层序遍历
-		//由于层序遍历是一种广度优先的遍历，所以我们需要通过层数控制整个函数的运行
-		//每轮循环传入要输出的层数
-		//然后当层数不等于0时，向下递归，每次递归层数减一
-		//当层数等于0的时候，则说明已经到达了要输出的那一层，递归的结束条件就满足了，直接输出值即可
-		void leavlOrder()
+		return vres;
+	}
+
+	vector<T> LevelNodeMax()//求每层节点的最大值
 	{
-			cout << "递归实现层序遍历:";
-			int l = leavl(_root);
-			for (int i = 0; i < l; ++i)
+		queue<TreeNode<T>*> qtmp;
+		vector<T> vres;
+		T Max;
+		int length = 1;
+		TreeNode<T>* cur = m_root;
+		if (!m_root)
+		{
+			return vres;
+		}
+		qtmp.push(cur);//先将根节点push进去
+
+		while (!qtmp.empty())
+		{
+			Max = qtmp.front()->m_val;
+			for (int i = 0; i < length; i++)//length为这层的节点数，一个for循环为一层遍历
 			{
-				leavlOrder(_root, i);
-			}
-			cout << endl;
+				cur = qtmp.front();
+				if (Max < qtmp.front()->m_val)
+				{
+					Max = qtmp.front()->m_val;
+				}
+				qtmp.pop();
+
+				if (cur->m_left)
+				{
+					qtmp.push(cur->m_left);
+				}
+
+				if (cur->m_right)
+				{
+					qtmp.push(cur->m_right);
+				}
+			}//for循环结束，就代表一层结束
+			vres.push_back(Max);
+			length = qtmp.size();
+		}
+		return vres;
+	}
+
+	vector<T> LevelNodeAverage()//求每层节点的平均值
+	{
+		queue<TreeNode<T>*> qtmp;
+		vector<T> vres;
+		T sum = 0;//一层节点值的总和，类型有可能为int float ,double用模板
+		T average;//一层节点平均值，这里使用模板类型，有其他需求可以改
+		int length = 1;//一层节点个数
+		TreeNode<T>* cur = m_root;
+
+		if (!m_root)
+		{
+			return vres;
 		}
 
-	void leavlOrder(BSTNode *node, int l)
-	{
-		if (node != nullptr)
+		qtmp.push(cur);//先将根节点push进去
+
+		while (!qtmp.empty())
 		{
-			if (l == 0)
+			for (int i = 0; i < length; i++)//length为这层的节点数，
 			{
-				cout << node->_data << " ";
-				return;
+				cur = qtmp.front();
+				sum += qtmp.front()->m_val;//将这层的所有结点值相加
+				qtmp.pop();
+
+				if (cur->m_left)
+				{
+					qtmp.push(cur->m_left);
+				}
+
+				if (cur->m_right)
+				{
+					qtmp.push(cur->m_right);
+				}
+			}//for循环结束，就代表一层结束
+			average = sum / length;//求这层平均值
+			sum = 0; //sum 清0，为下一层做准备
+			vres.push_back(average);
+			length = qtmp.size();//队列的长度就是下一层结点的个数
+		}
+		return vres;
+	}
+
+
+	//操作给定的二叉树，将其变换为源二叉树的镜像。
+	//void  Mirror(TreeNode<T>* root)//递归版
+	//{
+	//	if (!root)
+	//	{
+	//		return;
+	//	}
+
+
+	//}
+
+	void  Mirror()//非递归版
+	{
+		queue<TreeNode<T>*> qtmp;
+		if (!m_root)
+		{
+			return ;
+		}
+		TreeNode<T>* cur = m_root;
+		qtmp.push(cur);//先将根节点push进去
+		while (!qtmp.empty())
+		{
+			cur = qtmp.front();
+			swap(cur->m_left, cur->m_right);
+			qtmp.pop();
+			if (cur->m_left)
+			{
+				qtmp.push(cur->m_left);
 			}
-			leavlOrder(node->_left, l - 1);
-			leavlOrder(node->_right, l - 1);
+
+			if (cur->m_right)
+			{
+				qtmp.push(cur->m_right);
+			}
 		}
 	}
 
+	TreeNode<T>* lowestCommonAncestor(TreeNode<T>* p, TreeNode<T>* q)//最小公共祖先节点
+	{
+		TreeNode<T>* cur = m_root;
+		stack<TreeNode<T>*> stmp;
+		stack<bool> tag;
+		stack<TreeNode<T>*> s1;
+		stack<TreeNode<T>*> s2;
+		//此方法利用后序遍历 孩子比祖先先出的特点
+		if (!cur && !p && !q)
+		{
+			return nullptr;
+		}
+
+		do
+		{
+			for (; cur; cur = cur->m_left)//进所有的左孩子
+			{
+				stmp.push(cur);
+				tag.push(false);//访问标志位置false
+			}
+
+			while (!stmp.empty() && tag.top())
+			{
+				cur = stmp.top();//找到这个节点时，让这个s1栈等于这个stmp栈，他记录了从根节点到p节点的路径
+				//同样的方法,使s2栈也等于stmp,记录从根节点到q节点的路径
+				if (p == cur || q == cur)//s1和s2是节点的地址，所以是唯一的，不用担心重复
+				{
+					if (s1.empty())//谁为空给谁
+					{
+						s1 = stmp;
+					}
+
+					else
+					{
+						s2 = stmp;
+					}
+				}
+				stmp.pop();
+				tag.pop();
+			}
+
+			if (!stmp.empty())
+			{
+				cur = stmp.top()->m_right;
+				tag.top() = true;
+			}
+		} while (!stmp.empty());
+
+		if (s1.size() < s2.size())//使s1作为一个长的栈
+		{
+			swap(s1, s2);
+		}
+
+		int i;
+		int len = s1.size() - s2.size();
+		for (i = 0; i < len; i++)//让两个对其
+		{
+			s1.pop();
+		}
+		while (s1.top() != s2.top())//两个长度相等，找相同的那个祖先就好了
+		{
+			s1.pop();
+			s2.pop();
+		}
+		return s1.top();
+	}
 };
