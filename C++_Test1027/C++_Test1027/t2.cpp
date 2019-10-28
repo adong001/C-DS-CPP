f2aba3d8f54cb4b647338cac0ff8bdfcefdb5b8c
=‘E=ME=adong001 <1792095378@qq.com>
2019-10-28 21:35:18 +0800

    1

--- C++_Test1027/C++_Test1027/t2.cpp
+++ C++_Test1027/C++_Test1027/t2.cpp
@@ -1,6 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
 using namespace std;
+class Base
+{
+private:
+	int m_a;
+public:
+	virtual void Func()
+	{
+		cout << "Base" << endl;
+	}
+};
+
 
 int main2()
 {
