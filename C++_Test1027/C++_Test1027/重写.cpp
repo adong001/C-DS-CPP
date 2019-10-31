8f1967d1c3005254d04e29b335725e377cb97da4
=‘E=ME=adong001 <1792095378@qq.com>
2019-10-31 11:41:08 +0800

    1

--- C++_Test1027/C++_Test1027/重写.cpp
+++ C++_Test1027/C++_Test1027/重写.cpp
@@ -1,19 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
 using namespace std;
-class Base
-{
-private:
-	int m_a;
-public:
-	virtual void Func()
-	{
-		cout << "Base" << endl;
-	}
-};
 
-
-int main2()
+int main4()
 {
     system("pause");
     return 0;
