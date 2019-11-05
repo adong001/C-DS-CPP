46308400a1781892d6aec461544e2b75b26b9249
=‘E=ME=adong001 <1792095378@qq.com>
2019-11-05 22:11:41 +0800

    122

--- C++_Test1105(智能指针-删除器)/C++_Test1105(智能指针-删除器)/shared_ptr.cpp
+++ C++_Test1105(智能指针-删除器)/C++_Test1105(智能指针-删除器)/shared_ptr.cpp
@@ -6,4 +6,53 @@ int main3()
 {
     system("pause");
     return 0;
+}		auto_ptr<T>& operator =(auto_ptr<T>& aptr)
+{
+	if (m_ptr != aptr.m_ptr)//判断是不是自己给自己赋值
+	{
+		if (m_ptr)
+		{
+			delete m_ptr;
+		}
+		m_ptr = aptr.m_ptr;
+	}
+	return *this;
+}
+
+T& operator *()
+{
+	return *m_ptr;
+}
+
+T* operator ->()
+{
+	return m_ptr;
+}
+
+~auto_ptr()
+{
+	if (m_ptr)
+	{
+		delete m_ptr;
+	}
+	m_ptr = nullptr;
+	cout << "析构！" << endl;
+}
+	};
+};
+
+int main()
+{
+	int ar[] = { 1, 2, 3, 4, 5 };
+	char br[] = "hello";
+	Auto::auto_ptr<int> a1;
+	Auto::auto_ptr<char>b1;
+	a1 = ar;
+	b1 = br;
+	auto_ptr<T> r;
+
+	cout << *a1 << endl;
+	cout << *b1 << endl;
+	system("pause");
+	return 0;
 }
