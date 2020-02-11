790ffc8f4542ca6378c7614eacb2b08fb3578bb3
=‘E=ME=adong001 <1792095378@qq.com>
2020-02-11 17:48:30 +0800

    1

---
+++ C++_Test0210(开散列)/C++_Test0211(开散列)/main.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include<string>
+#include"HashSet.h"
+using namespace std;
+
+int main()
+{
+	YD::HashSet<int> hs;
+	hs.insert(1);
+	hs.insert(8);
+	hs.insert(13);
+	hs.insert(7);
+	hs.insert(2);
+	hs.insert(15);
+	hs.insert(18);
+	hs.insert(23);
+	hs.insert(17);
+	hs.insert(12);
+	hs.insert(25);
+	hs.insert(58);
+	hs.insert(33);
+	hs.insert(27);
+	hs.insert(22);
+
+	hs.erase(2);
+	hs.erase(27);
+
+	//hs.clear();
+
+    system("pause");
+    return 0;
+}
---
+++ C++_Test0211/C++_Test0211/HashBacket.h
@@ -0,0 +1,152 @@
+#pragma once
+#include <vector>
+using namespace std;
+
+template<class T>
+class HashBucketNode
+{
+	T m_val;
+	HashBucketNode<T> * m_next;
+
+	HashBucketNode(const T & val = T()) :
+		m_val(val),
+		m_next(nullptr)
+	{}
+
+	template<class T, class SW>
+	friend class HashSet;
+};
+
+class dealInt
+{
+public:
+	int operator()(int n)
+	{
+		return n;
+	}
+};
+
+template<class K,class V, class KeyOfValue,class SW = dealInt>
+class HashBucket 
+{
+	vector<HashBucketNode<T> *> m_data;
+	size_t m_size;
+
+	static long long s_m_primeTable[30];
+	int m_primePos;
+public:
+	HashSet(size_t capacity = s_m_primeTable[0]) :
+		m_data(capacity, nullptr),
+		m_size(0),
+		m_primePos(0)
+	{}
+
+private:
+	int hashFunc(const T & key)
+	{
+		SW func;
+		return func(key) % capacity();
+	}
+
+	void checkCapacity()
+	{
+		if (m_size == capacity())
+		{
+			int mcapa = capacity();
+			vector<HashBucketNode<T> *> tmp(s_m_primeTable[++m_primePos], nullptr);
+			m_data.swap(tmp);
+			m_size = 0;
+
+			int i;
+			HashBucketNode<T> * cur;
+			for (i = 0; i < mcapa; i++)
+			{
+				for (cur = tmp[i]; cur; cur = cur->m_next)
+				{
+					insert(cur->m_val);
+				}
+			}
+		}
+	}
+
+public:
+	bool insert(const T & val)
+	{
+		checkCapacity();
+
+		int hashnum = hashFunc(val);
+		HashBucketNode<T> * tmp;
+
+		if (m_data[hashnum])
+		{
+			for (tmp = m_data[hashnum]; tmp; tmp = tmp->m_next)
+			{
+				if (tmp->m_val == val)
+				{
+					return false;
+				}
+			}
+		}
+
+		tmp = new HashBucketNode<T>(val);
+
+		tmp->m_next = m_data[hashnum];
+		m_data[hashnum] = tmp;
+
+		m_size++;
+		return true;
+	}
+
+	bool erase(const T & val)
+	{
+		int hashnum = hashFunc(val);
+		HashBucketNode<T> * tmp;
+
+		if (!m_data[hashnum])
+		{
+			return false;
+		}
+
+		if (m_data[hashnum]->m_val == val)
+		{
+			tmp = m_data[hashnum];
+			m_data[hashnum] = tmp->m_next;
+			delete tmp;
+
+			m_size--;
+			return true;
+		}
+		else
+		{
+			for (tmp = m_data[hashnum]; tmp->m_next; tmp = tmp->m_next)
+			{
+				if (tmp->m_next->m_val == val)
+				{
+					HashBucketNode<T> * cur;
+					cur = tmp->m_next;
+					tmp->m_next = cur->m_next;
+					delete cur;
+
+					m_size--;
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+
+	size_t capacity()
+	{
+		return s_m_primeTable[m_primePos];
+	}
+};
+
+template<class T, class SW>
+long long HashSet<T, SW>::s_m_primeTable[30] = {
+	11, 23, 47, 89, 179,
+	353, 709, 1409, 2819, 5639,
+	11273, 22531, 45061, 90121, 180233,
+	360457, 720899, 1441807, 2883593, 5767169,
+	11534351, 23068673, 46137359, 92274737, 184549429,
+	369098771, 738197549, 1476395029, 2952790016u, 4294967291u
+};
