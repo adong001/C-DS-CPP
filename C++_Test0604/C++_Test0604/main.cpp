36746561f3e9eddc6d89d48ffab2dd62041adbe9
adong <1792095378@qq.com>
2020-06-04 20:37:22 +0800

    12

---
+++ C++_Test0604/C++_Test0604/main.cpp
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+
+//class A
+//{
+//private:
+//	int a;
+//public:
+//
+//	static int b;
+//};
+//int A::b = 0;
+class Date
+{
+public:
+	//Date(int year)
+	//	:_year(year)
+	//{}
+explicit Date(int year)
+:_year(year)
+{}
+
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+		void TestDate()
+	{
+			Date d1(2018);
+
+			// 用一个整形变量给日期类型对象赋值
+			// 实际编译器背后会用2019构造一个无名对象，最后用无名对象给d1对象进行赋值 
+			d1 = 2019;
+		}
+int main()
+{
+	TestDate();
+	//A::b = 0;
+	//cout << sizeof(A) << endl;
+    return 0;
+}
