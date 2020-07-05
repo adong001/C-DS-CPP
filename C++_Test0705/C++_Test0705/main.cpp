001b9f094fa78d8c6a11c55a7834484c01ba43ad
adong <1792095378@qq.com>
2020-07-05 10:42:24 +0800

    1

---
+++ C++_Test0705/C++_Test0705/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int n;
+	while (cin >> n)
+	{
+		int cnts = 0;
+		for (int i = 2; i <= sqrt(n); i++)
+		{
+			if (n % i == 0)
+			{
+				for (; n % i == 0; n /= i);
+				cnts++;
+			}
+		}
+		if (n != 1)
+		{
+			cnts++;
+		}
+		cout << cnts << endl;
+	}
+}
