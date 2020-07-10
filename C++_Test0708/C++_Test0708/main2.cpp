619152ed1c70331be8e4d8fcb4326b402aab60b6
adong <1792095378@qq.com>
2020-07-10 12:59:13 +0800

    1

--- C++_Test0708/C++_Test0708/main2.cpp
+++ C++_Test0708/C++_Test0708/main2.cpp
@@ -4,65 +4,88 @@
 #include <vector> 
 using namespace std;
 
-vector<bool> month_prime = { true, true, false, true, 
-								false, true, false, true,
-								true, false, true, false, true };
-vector<int> year_money = { 517, 486 };//非闰年/闰年一整年收益，
+int MonenyofMonth[] = { 29, 62, 28, 31, 60, 31, 60, 31, 62, 60, 62, 30, 62 };//每月转的钱
+int DayofMonth[] = { 29, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };//每月天数
+vector<bool> month_prime = { false, true, true, false, true, //是否为素数月
+							false, true, false, false, false, true, false };
+vector<int> year_money = { 580, 579 };//闰年/非闰年一整年收益，
+
 bool Isleap(int year)
 {
 	return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
 }
-int main2()
+
+int main()
 {
 	int y1, m1, d1, y2, m2, d2;
 	while (cin >> y1 >> m1 >> d1 >> y2 >> m2 >> d2)
 	{
-		int money1 = 0, money2 = 0, money3 = 0;
-		int f1 = Isleap(y1) ? 1 : 2;
-		int f2 = Isleap(y2) ? 1 : 2;
-		if (y1 == y2)
-		{
-			int i = 0;
-			if (m1 == m2)
-			{
-				if (m1 == 2)
-				{
-					money1 += ((d2 - d1) + 1) * f1;
-				}
-				else
-				{
-					money1 += (d2 - d1 + 1) * ((m1%2 != 0) + 1 || (m1 == 8) + 1);//在1-12月中%2!=0也是非素数
-				}
-			}
-			if (m1 <= 2)
-			{
+		int money = 0;
+		bool f1 = Isleap(y1);//是否为闰年
+		bool f2 = Isleap(y2);
 
-			}
-			for (; i < m2; i++)
-			{
-				money1 += month_prime[i];
-			}
-		}
-		else
+		if (y1 == y2 && m1 == m2)//同年同月
 		{
-			if (y1 <= 2)
+			money += month_prime[m1 - 1] ? (d2 - d1 + 1) : (d2 - d1 + 1) * 2;//非素数月就要*2
+		}
+		else if (y1 == y2 && m1 != m2)//同年不同月
+		{
+			//起始月
+			//(DayofMonth[m1] - d1 + 1) --这个月最后一天-起始天数+1就是这个月经营的总天数
+			//Isleap(y1) && m1 == 2) ? 1 : 0 --这个月是2月且这年是闰年这个月总天数就是29要+1
+			//month_prime[m1 - 1] ? 1 : 2 --这个月为非素数月就要*2
+			money += ((DayofMonth[m1] - d1 + 1) + ((f1 && m1 == 2) ? 1 : 0)) * (month_prime[m1 - 1] ? 1 : 2);
+			m1++;
+
+			//中间月
+			for (; m1 < m2; m1++)
 			{
-				int flag = 1;
-				money1 += Isleap(y1) ? month_prime[0]: month_prime[2], flag = 2;
-				if (y1 == 1)
+				if (m1 == 2 && f1)//闰年且2月，多一天
 				{
-					money1 = money1- d1 + 1;
+					money++;
 				}
-				else
-				{
-					money1 -= ( d1 + 1)* flag;
-				}
+				money += MonenyofMonth[m1];
 			}
-			for (int i = m1 + 1; i < 13; i++)
+
+			//结束月
+			money += d2  * (month_prime[m2 - 1] ? 1 : 2);
+		}
+		else //不同年不同月
+		{
+			//开始年
+			//开始月
+			money += ((DayofMonth[m1] - d1 + 1) + ((f1 && m1 == 2) ? 1 : 0)) * (month_prime[m1 - 1] ? 1 : 2);
+			m1++;
+			//中间月
+			for (; m1 <= 12; m1++)
 			{
-				money1 += month_prime[i];
+				if (m1 == 2 && f1)//闰年且2月，多一天
+				{
+					money++;
+				}
+				money += MonenyofMonth[m1];
+			}
+			y1++;
+
+			//中间年
+			for (; y1 < y2; y1++)
+			{
+				bool f = Isleap(y1);
+				money += Isleap(y1) ? year_money[0]: year_money[1];//区分是否为闰年即可
+			}
+
+			//结束年
+			money += d2 * (month_prime[m2 - 1] ? 1 : 2);
+			for (--m2; m2 > 0; m2--)
+			{
+				if (m2 == 2 && f2)//闰年且2月，多一天
+				{
+					money++;
+				}
+				money += MonenyofMonth[m2];
 			}
 		}
+		cout << money << endl;
 	}
     return 0;
 }
--- C++_Test0708/C++_Test0708/main3.cpp
+++ C++_Test0708/C++_Test0708/main3.cpp
@@ -1,31 +1,26 @@
-#define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
 using namespace std;
 
-int GetFib(int n)
-{
-	int f0 = 0, f1 = 1, f2 = 1, f;
-	while (n--)
-	{
-		f = f2;
-		f2 = (f1 + f2) % 1000000;
-		f0 = f1;
-		f1 = f;
-	}
-	return f1;
-}
-int main()
+int main3()
 {
 	int n;
 	while (cin >> n)
 	{
-		if (n < 29)
+		int tmp = n;
+		int f0 = 0, f1 = 1, f2 = 2;
+		while (n--)
 		{
-			printf("%d\n", GetFib(n));
+			f0 = f1 % 1000000;
+			f1 = f2 % 1000000;
+			f2 = f0 + f1;
+		}
+		if (tmp >= 30)
+		{
+			printf("%06d\n", f0);
 		}
 		else
 		{
-			printf("%06d\n", GetFib(n));
+			printf("%d\n", f0);
 		}
 	}
 	return 0;
