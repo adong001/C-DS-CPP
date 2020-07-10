ce48d91df3493b6b820a008b4b9b0e6286e06e41
adong <1792095378@qq.com>
2020-07-10 12:59:06 +0800

    12

---
+++ C++_Test0710/C++_Test0710/main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main1()
+{
+	string src, det;
+	while (cin >> src >> det)
+	{
+		int cnts = 0;
+		int i = 0;
+		while (size_t pos = src.find(det, i) != string::npos)
+		{
+			cnts++;
+			i = pos + det.size();
+		}
+		cout << cnts << endl;
+	}
+	return 0;
+}
---
+++ C++_Test0710/C++_Test0710/main1.cpp
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+int main()
+{
+	int n;
+	while (cin >> n)
+	{
+		string s;
+		getchar(); //Ë¢ÐÂ»º³åÇø
+		while(n--)
+		{
+			getline(cin , s);
+			bool flag = false;
+			if (s.find(',', 0) != string::npos || s.find(' ', 0) != string::npos)
+			{
+				flag = true;
+			}
+			if (flag)
+			{
+				cout << "\"" << s << "\"";
+			}
+			else
+			{
+				cout << s;
+			}
+			if (n == 0)
+			{
+				cout << endl;
+			}
+			else
+			{
+				cout << ", ";
+			}
+		}
+	}
+
+    return 0;
+}
