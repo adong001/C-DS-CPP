89a4832ddbc1d60a9de457ff7ac5dc2e87a877c2
adong <1792095378@qq.com>
2020-07-14 21:13:21 +0800

    12

---
+++ C++_Test0714/Project1/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int Getnum(int num)
+{
+	int cnts = 0;
+	int tmp = num;
+	do
+	{
+		if (num % 2 == 0)
+		{
+			num /= 2;
+		}
+		else
+		{
+			num = num * 3 + 1;
+		}
+		cnts++;
+	} while (num > tmp);
+	return cnts;
+}
+
+int main()
+{
+	int n;
+	while (cin >> n)
+	{
+		int maxnum = n, maxcnts = 0, tmp;
+		for (int i = 2; i <= n; i++)
+		{
+			tmp = Getnum(i);
+			if (maxcnts < tmp)
+			{
+				maxcnts = tmp;
+				maxnum = i;
+			}
+		}
+		cout << maxnum << endl << maxcnts << endl;
+	}
+}
